067787c53323c428b954968e60e9bfd5a3950213
Okcana Fedorova <kcenka4@gmail.com>
2024-12-07 20:37:28 +0300

    corect flake8 mypy

--- src/processing.py
+++ src/processing.py
@@ -4,13 +4,13 @@ from typing import Any, Iterable
 def filter_by_state(dictionaries: Iterable[dict[Any, Any]], state: Any = "EXECUTED") -> list[dict[Any, Any]]:
     """Функция для выведения данных по определенному значению"""
     executed_list = []
-    for i in dictionaries:
-        if i["state"] == state:
-            executed_list.append(i)
+    for index in dictionaries:
+        if index["state"] == state:
+            executed_list.append(index)
     return executed_list
 
 
-def sort_by_date(dictionaries: list[dict[str, Any]], revers: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(dictionaries: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     """Функция для сортировки по датам"""
-    list_for_date = sorted(dictionaries, key=lambda x: x["date"], reverse=revers)
+    list_for_date = sorted(dictionaries, key=lambda x: x["date"], reverse=reverse)
     return list_for_date
