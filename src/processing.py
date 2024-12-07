adc7f802bebfea4477eeed59ad63515dd1c0145f
Okcana Fedorova <kcenka4@gmail.com>
2024-12-07 16:24:38 +0300

    corect function2

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,6 @@
 from typing import Iterable, Any
 
-def filter_by_state(dictionaries: Iterable[list[dict[Any, Any]]]), state: Any='EXECUTED' -> list[list[dict[Any, Any]]]:
+def filter_by_state(dictionaries: Iterable[list[dict[Any, Any]]], state: Any='EXECUTED') -> list[list[dict[Any, Any]]]:
 """Функция для выведения данных по определенному значению"""
 executed_list = []
 for i in dictionaries:
