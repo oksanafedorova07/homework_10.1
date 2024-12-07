d306189f03b742aefbfec5025dc113e46c176c7b
Okcana Fedorova <kcenka4@gmail.com>
2024-12-07 17:22:37 +0300

    corect pip8

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,16 @@
-from typing import Iterable, Any
+from typing import Any, Iterable
 
 
-def filter_by_state(
-    dictionaries: Iterable[dict[Any, Any]], state: Any = "EXECUTED"
-) -> list[list[dict[Any, Any]]]:
+def filter_by_state(dictionaries: Iterable[dict[Any, Any]], state: Any = "EXECUTED") -> list[dict[Any, Any]]:
     """Функция для выведения данных по определенному значению"""
     executed_list = []
     for i in dictionaries:
         if i["state"] == state:
             executed_list.append(i)
-            return executed_list
+    return executed_list
 
 
-def sort_by_date(dictionaries: Iterable[list[dict[Any, Any]]], reverse: bool = True) -> list[list[dict[Any, Any]]]:
+def sort_by_date(dictionaries: list[dict[str, Any]], revers: bool = True) -> list[dict[str, Any]]:
     """Функция для сортировки по датам"""
-    list_for_date = sorted(dictionaries, key=lambda x: x["date"], reverse=reverse)
+    list_for_date = sorted(dictionaries, key=lambda x: x["date"], reverse=revers)
     return list_for_date
