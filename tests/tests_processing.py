a057de93c7204c435a5c9967376f8d6c9be12f74
Okcana Fedorova <kcenka4@gmail.com>
2024-12-15 00:40:45 +0300

    correct tests_processing.py

--- tests/tests_processing.py
+++ tests/tests_processing.py
@@ -1,21 +1,24 @@
 from src.processing import filter_by_state, sort_by_date
 
-def test_filter_by_state(test_dict_list_correct_date):
-    assert filter_by_state(test_dict_list_correct_date) == [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '30.06.2018'}]
 
-
-def test_filter_by_state(test_dict_list_not_correct_date):
-    assert filter_by_state(test_dict_list_not_correct_date) == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': 'gjfkuyojl'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '********'}
+def test_filter_by_state(dict_correct_date):
+    assert filter_by_state(dict_correct_date) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03"},
+        {"id": 939719570, "state": "EXECUTED", "date": "30.06.2018"},
     ]
 
-def test_sort_by_date(test_dict_list):
-    assert sort_by_date(test_dict_list) ==[
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+
+def test_filter_by_state1(dict_correct_date1):
+    assert filter_by_state(dict_correct_date1) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "gjfkuyojl"},
+        {"id": 939719570, "state": "EXECUTED", "date": "********"},
+    ]
+
+
+def test_sort_by_date(list_of_dicts):
+    assert sort_by_date(list_of_dicts) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
