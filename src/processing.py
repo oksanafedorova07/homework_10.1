bee276911ffca20917469c8e658ca4056863a42e
Okcana Fedorova <kcenka4@gmail.com>
2024-12-14 23:19:57 +0300

    corect processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,18 @@
-def filter_by_state(list_of_dicts: list, chosen_state: str = 'EXECUTED') -> list:
+def filter_by_state(list_of_dicts: list, chosen_state: str = "EXECUTED") -> list:
     """Функция которая принимает список словарей и опционально значение для ключа
-    state  (по умолчанию 'EXECUTED' ). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state  (по умолчанию 'EXECUTED' ). Функция возвращает новый список словарей, содержащий
+    только те словари, у которых ключ
     state  соответствует указанному значению."""
-   new_list_of_dicts = []
-   for dictionary in list_of_dicts:
-       if dictionary.get('state') == chosen_state:
-          new_list_of_dicts.append(dictionary)
-   return new_list_of_dicts
+    new_list_of_dicts = []
+    for dictionary in list_of_dicts:
+        if dictionary.get("state") == chosen_state:
+            new_list_of_dicts.append(dictionary)
+    return new_list_of_dicts
+
 
 def sort_by_date(list_of_dicts: list, is_reverse: bool = True) -> list:
-   """Функция ринимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (
-    date )."""
-    sort_list = sorted(list_of_dicts, key=lambda x: x['date'], reverse=is_reverse)
+    """Функция ринимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате
+    (date )."""
+    sort_list = sorted(list_of_dicts, key=lambda x: x["date"], reverse=is_reverse)
     return sort_list
-
