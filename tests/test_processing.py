1e336cddc6422fa94728a87c909f507f7030d1cb
Okcana Fedorova <kcenka4@gmail.com>
2024-12-15 11:23:06 +0300

    correct fail

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,21 +1,23 @@
+import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state(dict_correct_date):
+def test_filter_by_state(dict_correct_date: list) -> None:
     assert filter_by_state(dict_correct_date) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03"},
         {"id": 939719570, "state": "EXECUTED", "date": "30.06.2018"},
     ]
 
 
-def test_filter_by_state1(dict_correct_date1):
+def test_filter_by_state1(dict_correct_date1: list) -> None:
     assert filter_by_state(dict_correct_date1) == [
         {"id": 41428829, "state": "EXECUTED", "date": "gjfkuyojl"},
         {"id": 939719570, "state": "EXECUTED", "date": "********"},
     ]
 
 
-def test_sort_by_date(list_of_dicts):
+def test_sort_by_date(list_of_dicts: list) -> None:
     assert sort_by_date(list_of_dicts) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
