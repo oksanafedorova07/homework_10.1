467a2054f9d3d75a83f435433bf93c96a4a9ac22
Okcana Fedorova <kcenka4@gmail.com>
2024-12-19 13:39:18 +0300

    add test_generators.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,3 +25,14 @@ def list_of_dicts() -> list:
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
+
+
+@pytest.fixture()
+def trans_list() -> list:
+    return [
+        {"currency": {"name": "USD", "code": "USD"}},
+        {"currency": {"name": "USD", "code": "USD"}},
+        {"currency": {"name": "руб.", "code": "RUB"}},
+        {"currency": {"name": "USD", "code": "USD"}},
+        {"currency": {"name": "руб.", "code": "RUB"}}
+    ]
