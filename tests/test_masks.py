2b629a1769a76c96516bdcacf02cd9030d65d94f
Okcana Fedorova <kcenka4@gmail.com>
2025-01-14 00:00:28 +0300

    add correkt test

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,20 +1,25 @@
-import pytest  # noqa
+from typing import Any
+
+import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_mask_card_number() -> None:
-    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
-
-
-def test_mask_card_nunber_with_wrong_quantity() -> None:
-    assert get_mask_card_number("997000792289606361") != "Неправильно задан номер карты"
-    assert get_mask_card_number("70007922896063") != "Неправильно задан номер карты"
-
-
-def test_mask_card_number_empty() -> None:
-    assert get_mask_card_number(" ") != "Неправильно задан номер карты"
+@pytest.mark.parametrize(
+    "card, new_card",
+    [
+        ("", "Некорректный ввод"),
+        ("1234567890098765", "1234 56** **** 8765"),
+        ("123456789009834567765", "Некорректный ввод"),
+        ("qwertyuiop[];lk", "Некорректный ввод"),
+    ],
+)
+def test_get_mask_card_number(card: str, new_card: str) -> Any:
+    assert get_mask_card_number(card) == new_card
 
 
 def test_get_mask_account() -> None:
-    assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("12345678900987653456") == "**3456"
+    assert get_mask_account("1234567890098") == "Некорректный ввод"
+    assert get_mask_account("") == "Некорректный ввод"
+    assert get_mask_account("gfhgjhkfyu") == "Некорректный ввод"
