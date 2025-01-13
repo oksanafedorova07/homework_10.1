2190f9ed01bb8b70411a0f1fc72046be74cb07eb
Okcana Fedorova <kcenka4@gmail.com>
2025-01-13 23:46:22 +0300

    add log utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,46 @@
 import json
-from json import JSONDecodeError
+import logging
+import os
 from typing import Any
 
-from src.external_api import currency_conversion
+# Получаем абсолютный путь до текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Создаем путь до файла логов относительно текущей директории
+rel_log_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_log_file_path = os.path.abspath(rel_log_file_path)
+
+# Создаем путь до файла JSON относительно текущей директории
+rel_src_file_path = os.path.join(current_dir, "../data/operations.json")
+abs_src_file_path = os.path.abspath(rel_src_file_path)
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_log_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
-def financial_transactions(path: str) -> list:
-    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
-    try:
-        with open(path, encoding="utf-8") as financial_file:
-            try:
-                transactions = json.load(financial_file)
-            except JSONDecodeError:
-                return []
-        if not isinstance(transactions, list):
+def get_transactions_info_json(json_file: str) -> list[Any]:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей"""
+
+    with open(json_file, "r", encoding="utf-8") as file:
+        try:
+            logger.info("Путь до файла json верный")
+            transactions_info = json.load(file)
+            return transactions_info
+        except:
+            logger.warning("Импортируемый список пуст или отсутствует.")
             return []
-        return transactions
-    except FileNotFoundError:
-        return []
 
 
-def transaction_amount(trans: Any, currency: str = "RUB") -> Any:
-    """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
-    if trans["operationAmount"]["currency"]["code"] == currency:
-        amount = trans["operationAmount"]["amount"]
-        return amount
-    else:
-        amount = currency_conversion(trans)
-    return amount
+print(
+    json.dumps(
+        get_transactions_info_json(abs_src_file_path),
+        indent=4,
+    )
+)
