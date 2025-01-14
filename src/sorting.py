aa56fad36acab3b52f643632ce778cce71dfc037
Okcana Fedorova <kcenka4@gmail.com>
2025-01-15 00:40:34 +0300

    correct sorting.py

--- src/sorting.py
+++ src/sorting.py
@@ -11,3 +11,14 @@ def get_transactions_on_search_bar(data: list[dict], search_bar: str) -> list[di
             if re.findall(pattern, str(tr), flags=re.IGNORECASE):
                 transactions.append(transaction)
     return transactions
+
+
+def count_operations(data: list[dict], categories: list) -> dict:
+    """Возвращает словарь с количеством операций по категориям"""
+    common_categories = [category.lower() for category in categories]
+    operations_to_count = []
+    for transaction in data:
+        if str(transaction.get("description")).lower() in common_categories:
+            operations_to_count.append(transaction.get("description"))
+    result = Counter(operations_to_count)
+    return dict(result)
